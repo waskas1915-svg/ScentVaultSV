47d4e146935af70ff1e5309bd9abf1f36747f626
waskas1915-svg <waskas1915@gmail.com>
2026-04-01 08:18:27 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAW21g64pQ_sjPtv4BucApThJ3P8S1TqAI",
+    authDomain: "scentvaultsv.firebaseapp.com",
+    projectId: "scentvaultsv",
+    storageBucket: "scentvaultsv.firebasestorage.app",
+    messagingSenderId: "1087423307580",
+    appId: "1:1087423307580:web:f372b5937f332a55c90839"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  window.db = db; 
+
+
 let allProducts = [];
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
